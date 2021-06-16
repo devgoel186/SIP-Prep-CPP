f4c7cd2a4e9ead1deca3eb4b244b18a8801765f9
Dev Goel <goeldev01@gmail.com>
2021-06-16 19:57:27 +0530

    Added tree input depth wise

--- Trees/trees.cpp
+++ Trees/trees.cpp
@@ -14,9 +14,32 @@ struct Node
     }
 };
 
+Node<int> *takeInput()
+{
+    int rootData;
+    cout << "Enter Data" << endl;
+    cin >> rootData;
+    Node<int> *root = new Node<int>(rootData);
+
+    int n;
+    cout << "Enter Number of Children of " << rootData << endl;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        Node<int> *child = takeInput();
+        root->children.push_back(child);
+    }
+    return root;
+}
+
 void printTree(Node<int> *root)
 {
-    cout << root->data << endl;
+    cout << root->data << " : ";
+    for (int i = 0; i < root->children.size(); i++)
+    {
+        cout << root->children[i]->data << ", ";
+    }
+    cout << endl;
     for (int i = 0; i < root->children.size(); i++)
     {
         printTree(root->children[i]);
@@ -25,10 +48,12 @@ void printTree(Node<int> *root)
 
 int main()
 {
-    Node<int> *root = new Node<int>(1);
-    Node<int> *node1 = new Node<int>(2);
-    Node<int> *node2 = new Node<int>(3);
-    root->children.push_back(node1);
-    root->children.push_back(node2);
+    // Node<int> *root = new Node<int>(1);
+    // Node<int> *node1 = new Node<int>(2);
+    // Node<int> *node2 = new Node<int>(3);
+    // root->children.push_back(node1);
+    // root->children.push_back(node2);
+    // printTree(root);
+    Node<int> *root = takeInput();
     printTree(root);
 }

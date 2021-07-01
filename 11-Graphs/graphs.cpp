96ff18ce53e6113d616c0ac12969cef9e4991775
Dev Goel <goeldev01@gmail.com>
2021-07-01 21:24:24 +0530

    Added hasPath algo

--- 11-Graphs/graphs.cpp
+++ 11-Graphs/graphs.cpp
@@ -75,6 +75,43 @@ void BFS(int **edges, int n)
     delete[] visited;
 }
 
+bool hasPathHelper(int **edges, int n, int sv, int ev, bool *visited)
+{
+    visited[sv] = true;
+    bool res = false;
+
+    if (sv == ev)
+        return true;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (i == sv)
+            continue;
+        if (edges[sv][i] == 1)
+        {
+            if (visited[i])
+                continue;
+            res = hasPathHelper(edges, n, i, ev, visited);
+            if (res)
+                return res;
+        }
+    }
+    return res;
+}
+
+bool hasPath(int **edges, int n, int sv, int ev)
+{
+    bool *visited = new bool[n];
+    bool res = false;
+    for (int i = 0; i < n; i++)
+    {
+        visited[i] = false;
+    }
+    res = hasPathHelper(edges, n, sv, ev, visited);
+    delete[] visited;
+    return res;
+}
+
 void deleteMemory(int **edges, int n)
 {
     for (int i = 0; i < n; i++)
@@ -114,6 +151,8 @@ int main()
     cout << "BFS" << endl;
     BFS(edges, n);
     cout << endl;
+
+    cout << "Has Path ? = " << hasPath(edges, n, 0, 2) << endl;
 
     deleteMemory(edges, n);
 }

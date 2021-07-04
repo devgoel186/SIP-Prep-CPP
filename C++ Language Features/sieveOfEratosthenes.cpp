7ad6c58346850c10bb8fadf4b1eb53345e34985e
Dev Goel <goeldev01@gmail.com>
2021-07-04 17:58:19 +0530

    Added sieve of eratosthenes

---
+++ C++ Language Features/sieveOfEratosthenes.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void sieveLogic(int n)
+{
+    vector<int> arr(n, 0);
+
+    for (int i = 2; i <= n; i++)
+    {
+        if (arr[i] == 0)
+        {
+            for (int j = i * i; j <= n; j += i)
+            {
+                arr[j] = 1;
+            }
+        }
+    }
+    for (int i = 2; i <= n; i++)
+    {
+        if (arr[i] == 0)
+            cout << i << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    sieveLogic(n);
+}

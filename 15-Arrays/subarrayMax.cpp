32497cebec573ab78a1ff775e0ab01b9fe34544f
Dev Goel <goeldev01@gmail.com>
2021-07-12 18:06:55 +0530

    Added dp and kadane's problem circular subarray

--- 15-Arrays/subarrayMax.cpp
+++ 15-Arrays/subarrayMax.cpp
@@ -21,6 +21,20 @@ void printAllSubarrays(const vector<int> &a)
 int maxSubarray(const vector<int> &a)
 {
     int prevMax = INT_MIN, max = 0;
+    bool flag = false;
+    int maxNeg = INT_MIN;
+    for (int i = 0; i < a.size(); i++)
+    {
+        if (a[i] >= 0)
+        {
+            flag = true;
+            break;
+        }
+        if (a[i] > maxNeg)
+            maxNeg = a[i];
+    }
+    if (!flag)
+        return maxNeg;
     for (int i = 0; i < a.size(); i++)
     {
         max = max + a[i];
@@ -30,6 +44,26 @@ int maxSubarray(const vector<int> &a)
             max = 0;
     }
     return prevMax;
+}
+
+int maxCircularSubarray(vector<int> &a)
+{
+    int max_kadane = maxSubarray(a);
+    if (max_kadane < 0)
+        return max_kadane;
+
+    int max_wrap = 0;
+    for (int i = 0; i < a.size(); i++)
+    {
+        max_wrap += a[i];
+        a[i] = -a[i];
+    }
+
+    max_wrap = max_wrap + maxSubarray(a);
+
+    if (max_wrap > max_kadane)
+        return max_wrap;
+    return max_kadane;
 }
 
 // int maxSubarray2(const vector<int> &a)
@@ -62,6 +96,7 @@ int main()
     }
     cout << "Number of subarrays = " << n * (n + 1) / 2 << endl;
     printAllSubarrays(a);
-    cout << maxSubarray(a) << endl;
+    cout << "Max Subarray Sum = " << maxSubarray(a) << endl;
+    cout << "Max Circular Subarray Sum = " << maxCircularSubarray(a) << endl;
     // cout << maxSubarray2(a) << endl;
 }
---
+++ 16-DynamicProgramming/README.md
@@ -0,0 +1,4 @@
+# Dynamic Programming
+
+- Optimal Substructure - If a recurrence relation exists, our problem is an optimal substructure.
+- Overlapping Subproblem - If subproblems repeat, then a problem is said to have overlapping subproblem.

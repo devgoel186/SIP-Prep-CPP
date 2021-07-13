1adcaa8a03097c7c1abc323f9bdc50583e5b06db
devgoel186 <goeldev01@gmail.com>
2021-07-13 22:16:32 +0530

    Added bipartite graph question

--- 11-Graphs/bipartiteGraph.cpp
+++ 11-Graphs/bipartiteGraph.cpp
@@ -2,29 +2,23 @@
 using namespace std;
 #define ll long long
 
-void dfs(const vector<vector<int>> &adj, vector<bool> &visited, int node, vector<int> &color)
+bool isBipartite(const vector<vector<int>> &adj, vector<bool> &visited, int node, vector<int> &color)
 {
     visited[node] = true;
     for (int i = 0; i < adj.size(); i++)
     {
-        if (!visited[adj[node][i]] && adj[node][i] == 1)
+        if (i == node)
+            continue;
+        if (adj[node][i] == 1)
         {
-            color[adj[node][i]] = color[node] == 1 ? 0 : 1;
-            dfs(adj, visited, adj[node][i], color);
-        }
-    }
-}
-
-bool check(const vector<vector<int>> &adj, vector<bool> &visited, int node, vector<int> &color)
-{
-    visited[node] = true;
-    for (int i = 0; i < adj.size(); i++)
-    {
-        if (!visited[adj[node][i]] && adj[node][i] == 1)
-        {
-            if (color[adj[node][i]] == color[node])
+            if (!visited[i])
+            {
+                color[i] = color[node] xor 1;
+                if (!isBipartite(adj, visited, i, color))
+                    return false;
+            }
+            else if (color[i] == color[node])
                 return false;
-            dfs(adj, visited, adj[node][i], color);
         }
     }
     return true;
@@ -43,16 +37,28 @@ int main()
     {
         cin >> x >> y;
         adjl[x][y] = 1;
-        adjl[y][y] = 1;
+        adjl[y][x] = 1;
     }
 
-    color[0] = 1;
-    dfs(adjl, visited, 0, color);
-
+    bool flag = false;
     for (int i = 0; i < n; i++)
     {
-        visited[i] = false;
+        if (!visited[i])
+        {
+            color[i] = 0;
+            if (!isBipartite(adjl, visited, i, color))
+            {
+                flag = true;
+                break;
+            }
+        }
     }
 
-    cout << check(adjl, visited, 0, color);
+    flag ? cout << "Not Bipartite" << endl : cout << "Bipartite" << endl;
+
+    for (auto i : color)
+    {
+        cout << i << " ";
+    }
+    cout << endl;
 }
